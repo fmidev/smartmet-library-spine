bc22f3f44428783330018d27dbc20684dfa230e5
Heikki Pernu <pernu@fmi.fi>
2017-10-16 16:16:14 +0300

    First attempt at detecting unreadable config files

--- spine/Reactor.cpp
+++ spine/Reactor.cpp
@@ -40,6 +40,10 @@
 #include <stdexcept>
 #include <string>
 
+extern "C" {
+#include <unistd.h>
+}
+
 namespace fs = boost::filesystem;
 namespace ba = boost::algorithm;
 
@@ -56,6 +60,18 @@ void absolutize_path(std::string& file_name)
       file_name = path.string();
     }
   }
+}
+int is_file_readable(std::string& file_name)
+{
+  int handle;
+  // Apparently the only reliable way to test for readability is to actually open the file
+  // Use POSIX syscalls here for most efficient solution
+  if ((handle = open(file_name.c_str(), O_RDONLY)) >= 0)
+  {
+    close(handle);
+    return 0;
+  }
+  return errno;
 }
 }
 
@@ -567,7 +583,16 @@ bool Reactor::addPlugin(const std::string& theFilename, bool verbose)
     std::string configfile =
         itsOptions.itsConfig->get_optional_path("plugins." + pluginname + ".configfile", "");
     if (configfile != "")
+    {
       absolutize_path(configfile);
+
+      if (is_file_readable(configfile) != 0)
+      {
+        std::cerr << ANSI_FG_RED << "plugin " << pluginname << ": configuration file unreadable"
+                  << ANSI_FG_DEFAULT << std::endl;
+        throw SmartMet::Spine::Exception(BCP, configfile + ": " + std::strerror(errno));
+      }
+    }
 
     // Find the ip filters
     std::vector<std::string> filterTokens;
@@ -786,7 +811,15 @@ bool Reactor::loadEngine(const std::string& theFilename, bool verbose)
     std::string configfile =
         itsOptions.itsConfig->get_optional_path("engines." + enginename + ".configfile", "");
     if (configfile != "")
+    {
       absolutize_path(configfile);
+      if (is_file_readable(configfile) != 0)
+      {
+        std::cerr << ANSI_FG_RED << "engine " << enginename << ": configuration file unreadable"
+                  << ANSI_FG_DEFAULT << std::endl;
+        throw SmartMet::Spine::Exception(BCP, configfile + ": " + std::strerror(errno));
+      }
+    }
 
     itsEngineConfigs.insert(ConfigList::value_type(enginename, configfile));
 
