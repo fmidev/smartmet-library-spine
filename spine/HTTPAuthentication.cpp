30145d5d7a1d6de0ecc9cd447e5412d23c58eb7f
Mika Heiskanen <mika.heiskanen@fmi.fi>
2019-04-12 15:33:03 +0300

    Use static_cast instead of C-style cast

--- spine/HTTPAuthentication.cpp
+++ spine/HTTPAuthentication.cpp
@@ -1,120 +1,132 @@
 #include "HTTPAuthentication.h"
+#include "Exception.h"
 #include <boost/algorithm/string/case_conv.hpp>
 #include <boost/algorithm/string/predicate.hpp>
 #include <boost/algorithm/string/split.hpp>
 #include <macgyver/Base64.h>
-#include "Exception.h"
 
 namespace ba = boost::algorithm;
 
-namespace SmartMet {
-  namespace Spine {
-    namespace HTTP {
+namespace SmartMet
+{
+namespace Spine
+{
+namespace HTTP
+{
+Authentication::Authentication(bool denyByDefault) : denyByDefault(denyByDefault) {}
 
-      Authentication::Authentication(bool denyByDefault)
-	: denyByDefault(denyByDefault)
+Authentication::~Authentication() {}
+
+void Authentication::addUser(const std::string& name,
+                             const std::string& password,
+                             unsigned groupMask)
+{
+  userMap[name] = std::make_pair(password, groupMask);
+}
+
+bool Authentication::autheticateRequest(const Request& request, Response& response)
+{
+  try
+  {
+    auto credentials = request.getHeader("Authorization");
+
+    if (credentials)
+    {
+      std::vector<std::string> splitHeader;
+      ba::split(splitHeader, *credentials, ba::is_any_of(" "), ba::token_compress_on);
+      printf("Got credentials: %s\n", credentials->c_str());
+
+      if (splitHeader.size() < 2)
       {
+        // Corrupt Authorization header - 400 Bad request
+        badRequestResponse(response);
+        return false;
       }
 
-      Authentication::~Authentication()
+      if (ba::iequals(splitHeader.at(0), "basic"))
       {
+        auto givenDigest = splitHeader.at(1);
+        unsigned groupMask = getAutheticationGroup(request);
+        for (auto& item : userMap)
+        {
+          if ((item.second.second & groupMask) != 0)
+          {
+            auto trueDigest = Fmi::Base64::encode(item.first + ":" + item.second.first);
+            if (ba::iequals(trueDigest, givenDigest))
+            {
+              return true;
+            }
+          }
+        }
+        unauthorizedResponse(response);
+        return false;
+      }
+      else
+      {
+        // Not supported or invalid authentication type
+        badRequestResponse(response);
+        return false;
+      }
+    }
+    else
+    {
+      printf("No credetials available\n");
+      // Ask whether authentication is required for request if not provided there
+      if (not isAutheticationRequired(request))
+      {
+        return true;
       }
 
-      void Authentication::addUser(const std::string& name, const std::string& password, unsigned groupMask)
+      if (userMap.empty() and not denyByDefault)
       {
-	userMap[name] = std::make_pair(password, groupMask);
+        return true;
       }
 
-      bool Authentication::autheticateRequest(const Request& request, Response& response)
-      {
-	try {
-	  auto credentials = request.getHeader("Authorization");
+      unauthorizedResponse(response);
+      return false;
+    }
+  }
+  catch (...)
+  {
+    throw SmartMet::Spine::Exception::Trace(BCP, "Operation failed!");
+  }
+}
 
-	  if (credentials) {
-	    std::vector<std::string> splitHeader;
-	    ba::split(splitHeader, *credentials, ba::is_any_of(" "), ba::token_compress_on);
-	    printf("Got credentials: %s\n", credentials->c_str());
+bool Authentication::isAutheticationRequired(const Request& request) const
+{
+  (void)request;
+  return true;
+}
 
-	    if (splitHeader.size() < 2) {
-	      // Corrupt Authorization header - 400 Bad request
-	      badRequestResponse(response);
-	      return false;
-	    }
+unsigned Authentication::getAutheticationGroup(const Request& request) const
+{
+  (void)request;
+  return static_cast<unsigned>(-1);
+}
 
-	    if (ba::iequals(splitHeader.at(0), "basic")) {
-	      auto givenDigest = splitHeader.at(1);
-	      unsigned groupMask = getAutheticationGroup(request);
-	      for (auto& item : userMap) {
-		if ((item.second.second & groupMask) != 0) {
-		  auto trueDigest = Fmi::Base64::encode(item.first + ":" + item.second.first);
-		  if (ba::iequals(trueDigest, givenDigest)) {
-		    return true;
-		  }
-		}
-	      }
-	      unauthorizedResponse(response);
-	      return false;
-	    } else {
-	      // Not supported or invalid authentication type
-	      badRequestResponse(response);
-	      return false;
-	    }
-	  } else {
-	    printf("No credetials available\n");
-	    // Ask whether authentication is required for request if not provided there
-	    if (not isAutheticationRequired(request)) {
-	      return true;
-	    }
+std::string Authentication::getRealm() const
+{
+  return "SmartMet server";
+}
 
-	    if (userMap.empty() and not denyByDefault) {
-	      return true;
-	    }
+void Authentication::unauthorizedResponse(Response& response)
+{
+  response.setStatus(Status::unauthorized);
+  response.setHeader("WWW-Authenticate", "Basic realm=\"" + getRealm() + "\"");
+  response.setHeader("Content-Type", "text/html; charset=UTF-8");
+  const std::string content = "<html><body><h1>401 Unauthorized </h1></body></html>\n";
+  response.setContent(content);
+}
 
-	    unauthorizedResponse(response);
-	    return false;
-	  }
-	}
-	catch (...)
-	  {
-	    throw SmartMet::Spine::Exception::Trace(BCP, "Operation failed!");
-	  }
-      }
+void Authentication::badRequestResponse(Response& response)
+{
+  response.setStatus(Status::bad_request);
+  response.setHeader("WWW-Authenticate", "Basic realm=\"" + getRealm() + "\"");
+  response.setHeader("Content-Type", "text/html; charset=UTF-8");
+  const std::string content = "<html><body><h1>400 Bad request </h1></body></html>\n";
+  response.setContent(content);
+}
 
-      bool Authentication::isAutheticationRequired(const Request& request) const
-      {
-	(void)request;
-	return true;
-      }
-
-      unsigned Authentication::getAutheticationGroup(const Request& request) const
-      {
-	(void)request;
-	return (unsigned)-1;
-      }
-
-      std::string Authentication::getRealm() const
-      {
-	return "SmartMet server";
-      }
-
-      void Authentication::unauthorizedResponse(Response& response)
-      {
-	  response.setStatus(Status::unauthorized);
-	  response.setHeader("WWW-Authenticate", "Basic realm=\"" + getRealm() + "\"");
-	  response.setHeader("Content-Type", "text/html; charset=UTF-8");
-	  const std::string content = "<html><body><h1>401 Unauthorized </h1></body></html>\n";
-	  response.setContent(content);
-      }
-
-      void Authentication::badRequestResponse(Response& response)
-      {
-	  response.setStatus(Status::bad_request);
-	  response.setHeader("WWW-Authenticate", "Basic realm=\"" + getRealm() + "\"");
-	  response.setHeader("Content-Type", "text/html; charset=UTF-8");
-	  const std::string content = "<html><body><h1>400 Bad request </h1></body></html>\n";
-	  response.setContent(content);
-      }
-
-    } // namespace HTTP
-  } // namespace Spine
-} // namespace SmartMet
+}  // namespace HTTP
+}  // namespace Spine
+}  // namespace SmartMet
