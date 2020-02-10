7bf3eb4de045fb8b2c51c612e29d12c5ae6b5186
Andris Pavenis <andris.pavenis@fmi.fi>
2020-02-10 09:55:57 +0200

    Import class MultiLanguageString from WFS plugin together with its unit tests

---
+++ spine/MultiLanguageString.h
@@ -0,0 +1,36 @@
+#pragma once
+
+#include <spine/ConfigBase.h>
+#include <map>
+#include <memory>
+#include <string>
+
+namespace SmartMet
+{
+namespace Spine
+{
+class MultiLanguageString
+{
+ public:
+  MultiLanguageString(const std::string& default_language, libconfig::Setting& setting);
+
+  static std::shared_ptr<MultiLanguageString> create(const std::string& default_language,
+                                                       libconfig::Setting& setting);
+
+  virtual ~MultiLanguageString();
+
+  inline std::string get_default_language() const { return default_language; }
+  inline std::string get() const { return get(default_language); }
+  std::string get(const std::string& language) const;
+
+  inline const std::map<std::string, std::string>& get_content() const { return data; }
+
+ private:
+  const std::string default_language;
+  std::map<std::string, std::string> data;
+};
+
+typedef std::shared_ptr<MultiLanguageString> MultiLanguageStringP;
+
+}  // namespace Spine
+}  // namespace SmartMet
