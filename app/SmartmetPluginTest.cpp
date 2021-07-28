913937776ecb5d16d7af585bbdb6d558f0bfa6c3
Mika Heiskanen <mika.heiskanen@fmi.fi>
2021-07-28 10:22:39 +0300

    Prefer nullptr over NULL

--- app/SmartmetPluginTest.cpp
+++ app/SmartmetPluginTest.cpp
@@ -1,7 +1,7 @@
 #include "PluginTest.h"
+#include <boost/program_options.hpp>
 #include <iostream>
 #include <sstream>
-#include <boost/program_options.hpp>
 
 using namespace std;
 
@@ -18,27 +18,25 @@ void prelude(SmartMet::Spine::Reactor& reactor, const std::string& handler_path)
       handlers = reactor.getURIMap();
     }
 
-    if (timeout < 0) {
-        throw Fmi::Exception::Trace(BCP, "Timed out waiting for plugin to start");
+    if (timeout < 0)
+    {
+      throw Fmi::Exception::Trace(BCP, "Timed out waiting for plugin to start");
     }
 
-    cout << endl << "Testing " << handlers[handler_path] << "  plugin"
-         << std::endl << "============================"
-         << std::endl;
+    cout << endl
+         << "Testing " << handlers[handler_path] << "  plugin" << std::endl
+         << "============================" << std::endl;
   }
   catch (...)
   {
-    throw Fmi::Exception(BCP, "Prelude failed!", NULL);
+    throw Fmi::Exception(BCP, "Prelude failed!", nullptr);
   }
 }
 
 void alarm_handler(int)
 {
-    std::cerr << std::endl
-              << "Timeout: test terminated"
-              << std::endl
-              << std::endl;
-    abort();
+  std::cerr << std::endl << "Timeout: test terminated" << std::endl << std::endl;
+  abort();
 }
 
 #define nm_help "help"
@@ -50,7 +48,6 @@ void alarm_handler(int)
 #define nm_ignore "ignore"
 #define nm_timeout "timeout"
 #define nm_handler "handler"
-
 
 int main(int argc, char* argv[])
 {
@@ -66,38 +63,123 @@ int main(int argc, char* argv[])
 
     po::options_description desc("Allowed options");
 
-    desc.add_options()
-        (nm_help ",h", "Show this help message")
-        (nm_handler ",H", po::value<std::string>(), "Handler path (for example '/wfs'")
-        (nm_config ",c", po::value<std::string>(), "Reactor config file")
-        (nm_input ",i", po::value<std::string>(), "Tests input directory (default 'input')")
-        (nm_expected ",e", po::value<std::string>(), "Expected output directory (default 'output')")
-        (nm_failures ",f", po::value<std::string>(), "Directory where to write actual output in"
-            " case of failures (default value 'failures')")
-        (nm_threads ",n", po::value<int>(), "Number of threads (default 10)")
-        (nm_ignore ",I", po::value<std::vector<std::string> >(),
-            "Optional parameter to specify files containing lists"
-            " of tests to be skipped. File .testignore from input directory is used of none specified."
-            " May be provided 0 or more times")
-        (nm_timeout, po::value<unsigned>(), "Timeout of entire test run in seconds (missing or value "
-            " 0 means no timeout")
-        ;
+    desc.add_options()(nm_help ",h", "Show this help message")(
+        nm_handler ",H",
+        po::value<std::string>(),
+        "Handler path (for example '/wfs'")(nm_config ",c",
+                                            po::value<std::string>(),
+                                            "Reactor config file")(nm_input ",i",
+                                                                   po::value<std::string>(),
+                                                                   "Tests input directory (default "
+                                                                   "'input')")(nm_expected ",e",
+                                                                               po::value<
+                                                                                   std::string>(),
+                                                                               "Expected output "
+                                                                               "directory (default "
+                                                                               "'output'"
+                                                                               ")")(nm_failures
+                                                                                    ",f",
+                                                                                    po::value<
+                                                                                        std::
+                                                                                            string>(),
+                                                                                    "Directory "
+                                                                                    "where to "
+                                                                                    "write actual "
+                                                                                    "output in"
+                                                                                    " case of "
+                                                                                    "failures "
+                                                                                    "(default "
+                                                                                    "value "
+                                                                                    "'failures'"
+                                                                                    ")")(nm_threads
+                                                                                         ",n",
+                                                                                         po::value<
+                                                                                             int>(),
+                                                                                         "Number "
+                                                                                         "of "
+                                                                                         "threads "
+                                                                                         "(default "
+                                                                                         "10"
+                                                                                         ")")(nm_ignore
+                                                                                              ",I",
+                                                                                              po::value<std::vector<
+                                                                                                  std::
+                                                                                                      string> >(),
+                                                                                              "Opti"
+                                                                                              "onal"
+                                                                                              " par"
+                                                                                              "amet"
+                                                                                              "er "
+                                                                                              "to "
+                                                                                              "spec"
+                                                                                              "ify "
+                                                                                              "file"
+                                                                                              "s "
+                                                                                              "cont"
+                                                                                              "aini"
+                                                                                              "ng "
+                                                                                              "list"
+                                                                                              "s"
+                                                                                              " of "
+                                                                                              "test"
+                                                                                              "s "
+                                                                                              "to "
+                                                                                              "be "
+                                                                                              "skip"
+                                                                                              "ped."
+                                                                                              " Fil"
+                                                                                              "e "
+                                                                                              ".tes"
+                                                                                              "tign"
+                                                                                              "ore "
+                                                                                              "from"
+                                                                                              " inp"
+                                                                                              "ut "
+                                                                                              "dire"
+                                                                                              "ctor"
+                                                                                              "y "
+                                                                                              "is "
+                                                                                              "used"
+                                                                                              " of "
+                                                                                              "none"
+                                                                                              " spe"
+                                                                                              "cifi"
+                                                                                              "ed."
+                                                                                              " May"
+                                                                                              " be "
+                                                                                              "prov"
+                                                                                              "ided"
+                                                                                              " 0 "
+                                                                                              "or "
+                                                                                              "more"
+                                                                                              " tim"
+                                                                                              "e"
+                                                                                              "s")(nm_timeout,
+                                                                                                   po::value<
+                                                                                                       unsigned>(),
+                                                                                                   "Timeout of entire test run in seconds (missing or value "
+                                                                                                   " 0 means no timeout");
 
     po::variables_map opt;
     po::store(po::parse_command_line(argc, argv, desc), opt);
 
     po::notify(opt);
 
-    if (opt.count(nm_help)) {
-        std::cout << desc << std::endl;
-        exit(1);
+    if (opt.count(nm_help))
+    {
+      std::cout << desc << std::endl;
+      exit(1);
     }
 
     SmartMet::Spine::Options options;
-    if (opt.count(nm_config)) {
-        options.configfile = opt[nm_config].as<std::string>();
-    } else {
-        throw Fmi::Exception::Trace(BCP, "Mandatory command line option --reactor-config (or -c) missing");
+    if (opt.count(nm_config))
+    {
+      options.configfile = opt[nm_config].as<std::string>();
+    }
+    else
+    {
+      throw Fmi::Exception::Trace(BCP,
+                                  "Mandatory command line option --reactor-config (or -c) missing");
     }
     options.quiet = true;
     options.defaultlogging = false;
@@ -107,38 +189,49 @@ int main(int argc, char* argv[])
 
     try
     {
-        SmartMet::Spine::PluginTest tester;
-        SmartMet::Spine::PreludeFunction prelude_funct;
-        if (opt.count(nm_input)) {
-            tester.setInputDir(opt[nm_input].as<std::string>());
-        }
-        if (opt.count(nm_timeout)) {
-            alarm(opt[nm_timeout].as<unsigned>());
-        }
-        if (opt.count(nm_expected)) {
-            tester.setOutputDir(opt[nm_expected].as<std::string>());
-        }
-        if (opt.count(nm_failures)) {
-            tester.setFailDir(opt[nm_failures].as<std::string>());
-        }
-        if (opt.count(nm_threads)) {
-            tester.setNumberOfThreads(opt[nm_threads].as<int>());
-        }
-        if (opt.count(nm_ignore)) {
-            ignore_lists = opt[nm_ignore].as<std::vector<std::string> >();
-        }
-        if (opt.count(nm_handler)) {
-            const std::string handler_path = opt[nm_handler].as<std::string>();
-            prelude_funct = [handler_path](SmartMet::Spine::Reactor& reactor) { prelude(reactor, handler_path); };
-        } else {
-            throw Fmi::Exception::Trace(BCP, "Mandatory command line option --handler (-H) missing");
-        }
+      SmartMet::Spine::PluginTest tester;
+      SmartMet::Spine::PreludeFunction prelude_funct;
+      if (opt.count(nm_input))
+      {
+        tester.setInputDir(opt[nm_input].as<std::string>());
+      }
+      if (opt.count(nm_timeout))
+      {
+        alarm(opt[nm_timeout].as<unsigned>());
+      }
+      if (opt.count(nm_expected))
+      {
+        tester.setOutputDir(opt[nm_expected].as<std::string>());
+      }
+      if (opt.count(nm_failures))
+      {
+        tester.setFailDir(opt[nm_failures].as<std::string>());
+      }
+      if (opt.count(nm_threads))
+      {
+        tester.setNumberOfThreads(opt[nm_threads].as<int>());
+      }
+      if (opt.count(nm_ignore))
+      {
+        ignore_lists = opt[nm_ignore].as<std::vector<std::string> >();
+      }
+      if (opt.count(nm_handler))
+      {
+        const std::string handler_path = opt[nm_handler].as<std::string>();
+        prelude_funct = [handler_path](SmartMet::Spine::Reactor& reactor)
+        { prelude(reactor, handler_path); };
+      }
+      else
+      {
+        throw Fmi::Exception::Trace(BCP, "Mandatory command line option --handler (-H) missing");
+      }
 
-        for (const auto& fn : ignore_lists) {
-            tester.addIgnoreList(fn);
-        }
+      for (const auto& fn : ignore_lists)
+      {
+        tester.addIgnoreList(fn);
+      }
 
-        return tester.run(options, prelude_funct);
+      return tester.run(options, prelude_funct);
     }
     catch (const libconfig::ParseException& err)
     {
@@ -151,7 +244,7 @@ int main(int argc, char* argv[])
   }
   catch (...)
   {
-    throw Fmi::Exception(BCP, "Operation failed!", NULL);
+    throw Fmi::Exception(BCP, "Operation failed!", nullptr);
   }
 
   return 1;
