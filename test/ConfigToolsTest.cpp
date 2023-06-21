7357471ec162cca78100fe2b1801d780ac4327d5
Mika Heiskanen <mika.heiskanen@fmi.fi>
2023-06-21 14:30:18 +0300

    Added some tests for ConfigTools

---
+++ test/ConfigToolsTest.cpp
@@ -0,0 +1,61 @@
+#include "ConfigTools.h"
+#include <boost/test/included/unit_test.hpp>
+// #include <cstdlib>
+// #include <cstring>
+
+using namespace boost::unit_test;
+
+test_suite* init_unit_test_suite(int argc, char* argv[])
+{
+  const char* name = "ConfigBase tester";
+  unit_test_log.set_threshold_level(log_messages);
+  framework::master_test_suite().p_name.value = name;
+  BOOST_TEST_MESSAGE("");
+  BOOST_TEST_MESSAGE(name);
+  BOOST_TEST_MESSAGE(std::string(std::strlen(name), '='));
+  return NULL;
+}
+
+namespace
+{
+class TestConfig : public libconfig::Config
+{
+ public:
+  TestConfig()
+  {
+    using libconfig::Setting;
+    auto& root = getRoot();
+    root.add("ROOT", Setting::TypeString) = "/etc";
+    root.add("BASEDIR", Setting::TypeString) = "$(ROOT)/base";
+    root.add("USER", Setting::TypeString) = "$(USER)";
+  }
+};
+
+boost::shared_ptr<libconfig::Config> create_config()
+{
+  boost::shared_ptr<libconfig::Config> config(new TestConfig);
+  SmartMet::Spine::expandVariables(*config);
+  return config;
+}
+}  // namespace
+
+BOOST_AUTO_TEST_CASE(expansion)
+{
+  using namespace SmartMet;
+  using libconfig::Setting;
+
+  BOOST_TEST_MESSAGE("+ [ConfigTools] Testing variable expansion");
+
+  auto cfg = create_config();
+
+  std::string value;
+
+  BOOST_REQUIRE_NO_THROW(value = cfg->lookup("ROOT").c_str());
+  BOOST_CHECK_EQUAL("/etc", value);
+
+  BOOST_REQUIRE_NO_THROW(value = cfg->lookup("BASEDIR").c_str());
+  BOOST_CHECK_EQUAL("/etc/base", value);
+
+  BOOST_REQUIRE_NO_THROW(value = cfg->lookup("USER").c_str());
+  BOOST_CHECK_NE("", value);
+}
